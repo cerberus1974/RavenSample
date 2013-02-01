625e43b41b1137c693dcaaabcf1261babe3e9ca9
cerberus1974 <cerberus1974@gmail.com>
2013-02-01 22:52:31 +0900

    RavenDBを追加 Addメソッド作成

---
+++ RavenSample/Controllers/CommentController.cs
@@ -0,0 +1,45 @@
+﻿using Raven.Client;
+using Raven.Client.Embedded;
+using RavenSample.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RavenSample.Controllers
+{
+    public class CommentController : Controller
+    {
+        //
+        // GET: /Comment/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Add()
+        {
+            using (IDocumentStore instance = new EmbeddableDocumentStore { DataDirectory = @"~\Database" })
+            {
+                instance.Initialize();
+
+                using (IDocumentSession session = instance.OpenSession())
+                {
+                    var comment = new Comment
+                    {
+                        Content = "test",
+                        Commenter = "hogehoge",
+                        CreateAt = DateTime.Now,
+                    };
+
+                    session.Store(comment);
+                    session.SaveChanges();
+                }
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ RavenSample/Models/Comment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RavenSample.Models
+{
+    public class Comment
+    {
+        public int ID { get; set; }
+        public string Content { get; set; }
+        public string Commenter { get; set; }
+        public DateTime CreateAt { get; set; }
+    }
+}
